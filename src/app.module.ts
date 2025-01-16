e5fe15326fd4a4d80fe07897239b1eb22e7989d7
Suphanat Natthakul <littlestrike09@gmail.com>
2025-01-16 16:39:52 +0700

    learn: Nest.js beginner04 DTO & Nest pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
-// Importing the Module decorator from NestJS's common module set.
 import { Module } from '@nestjs/common';
-
-// Importing the AuthModule, UserModule, and BookmarkModule from their respective paths.
-// These modules might be responsible for handling authentication, user data, and bookmark functionality.
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-// The @Module decorator is used to define AppModule as a NestJS module.
-// It helps in organizing the application by grouping related functionalities.
 @Module({
-  // Listing modules that AppModule will use. This establishes AppModule's dependencies on these modules.
-  // It means that AppModule can utilize functionalities exported from these imported modules.
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
-export class AppModule {} // Defining the AppModule class that brings together the functionality of the imported modules.
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
-// Importing NestFactory, which is a core part of NestJS used for creating application instances.
 import { NestFactory } from '@nestjs/core';
-// Importing AppModule, the root module of the application, which organizes all the application features.
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-// Defining an asynchronous function called 'bootstrap' to initialize and start the server.
 async function bootstrap() {
-  // Using NestFactory to create an instance of the application based on AppModule.
-  // This setup process includes setting up the entire dependency graph of modules and providers.
   const app = await NestFactory.create(AppModule);
-
-  // Telling the created app instance to listen on port 3333.
-  // This starts the server and makes it listen for incoming requests on this port.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 
-// Calling the bootstrap function to execute the initialization and start the server.
 bootstrap();
