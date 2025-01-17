d0346c830ce91b42a0cbceb9136898282f2b03e4
Suphanat Natthakul <littlestrike09@gmail.com>
2025-01-17 14:31:09 +0700

    learn: Nest.js beginner06 Sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -33,9 +33,28 @@ export class AuthService {
       }
     }
   }
-  signin() {
-    return {
-      message: 'I am signin test',
-    };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    //if password is incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    delete user.hash;
+
+    //send back the user
+    return user;
   }
 }
